51f43854667584b81be8f179a830210013e6c5cd
Vaibhav55072 <147960856+Vaibhav55072@users.noreply.github.com>
2024-01-17 18:20:50 +0530

    NodejsFunctions.js

--- NodejsFunctions.js
+++ NodejsFunctions.js
@@ -73,13 +73,6 @@ function area(pi=3.14,r){
     console.log("The Area of Circle is"+area()) ; //Here the function is called
 
     
-// Function to calculate the Area of circle
-//Creating an function named as area
-//FORMULA : pi*r*r
-function area(pi=3.14,r){
-    return pi*r*r;   // Here the calculations is stopped and return the values
-    }
-    var promt=("Enter the Raidus:");
-    console.log("The Area of Circle is"+area()) ; //Here the function is called
+
 
     
