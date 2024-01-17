9f04cf1400e2f61f1d144f8a01e7804eaaf0f02a
Vaibhav55072 <147960856+Vaibhav55072@users.noreply.github.com>
2024-01-17 18:09:01 +0530

    Add files via upload

---
+++ NodejsFunctions.js
@@ -0,0 +1,85 @@
+// Function For Addition of 2 Numbers
+//Creating an function named as addition 
+
+function addition(x,y){
+return x+y;     // Here the calculations is stopped and return the values
+}
+addition(10,30)     //Here the function is called
+
+
+
+// Function For Substraction of 2 Numbers
+//Creating an function named as sub
+
+function sub(a,b){
+    return a-b;     // Here the calculations is stopped and return the values
+    }
+    sub(100,50)     //Here the function is called
+    
+
+
+    // Function For Multiplication of 2 Numbers.
+//Creating an function named as multiply. 
+
+function multiply(x,y){
+    return x*y;     // Here the calculations is stopped and return the values.
+    }
+    multiply(10,30)     //Here the function is called.
+    
+
+
+    
+    // Function For division of 2 Numbers.
+//Creating an function named as div. 
+
+function div(x,y){
+    return x/y;     // Here the calculations is stopped and return the values.
+    }
+    div(10,2)     //Here the function is called.
+    
+
+    
+    // Function to find factorial of  Numbers.
+//Creating an function named as factorial. 
+
+function factorial(num){
+    return (num*factorial(num-1));     // Here the calculations is stopped and  it return the values.
+    }
+    factorial(10)     //Here the function is called.
+    
+
+    
+    // Function to check  number is Even or Odd 
+//Creating an function named as evenodd 
+
+function evenodd(number){
+    var number=promt("Enter the number :") // Taking the input from the user
+    if(number%2==0) //if-else statement is used to check and return the statement which is true
+   {
+console.log("The number is Even");
+else
+console.log("The number is Odd")
+}} ; 
+    
+
+
+// Function to calculate the Area of circle
+//Creating an function named as area
+//FORMULA : pi*r*r
+function area(pi=3.14,r){
+    return pi*r*r;   // Here the calculations is stopped and return the values
+    }
+    var promt=("Enter the Raidus:");
+    console.log("The Area of Circle is"+area()) ; //Here the function is called
+
+    
+// Function to calculate the Area of circle
+//Creating an function named as area
+//FORMULA : pi*r*r
+function area(pi=3.14,r){
+    return pi*r*r;   // Here the calculations is stopped and return the values
+    }
+    var promt=("Enter the Raidus:");
+    console.log("The Area of Circle is"+area()) ; //Here the function is called
+
+    
